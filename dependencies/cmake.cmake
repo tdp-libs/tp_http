3a778ecaba28e92dac2018455672dabfe2b9d9d8
Tom Paynter <tompaynter@tdpe.co.uk>
2020-05-13 15:28:06 +0100

    Fixing windows cmake linker issues.

--- dependencies/cmake.cmake
+++ dependencies/cmake.cmake
@@ -1,6 +1,6 @@
 if(WIN32)
-  list(APPEND TP_LIBRARIES "libssl")
-  list(APPEND TP_LIBRARIES "libcrypto")
+  list(APPEND TP_LIBRARIES "libeay32")
+  list(APPEND TP_LIBRARIES "ssleay32")
 else()
   list(APPEND TP_LIBRARIES "ssl")
   list(APPEND TP_LIBRARIES "crypto")
